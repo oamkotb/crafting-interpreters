9ee261a95d3107aa732ccaa2f5c87a008e36176f
oamkotb <omar.elmanfaluty@gmail.com>
2024-08-07 16:40:05 -0600

    chapter 8 complete

--- jlox/lox/Interpreter.java
+++ jlox/lox/Interpreter.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Interpreter implements Expr.Visitor<Object>, 
                                     Stmt.Visitor<Void>
 {
+  private Environment environment = new Environment();
+
   public void interpret(List<Stmt> statements)
   {
     try
@@ -47,6 +49,12 @@ public class Interpreter implements Expr.Visitor<Object>,
       default:
         return null;
     }
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr)
+  {
+    return environment.get(expr.name);
   }
 
   @Override
@@ -143,6 +151,49 @@ public class Interpreter implements Expr.Visitor<Object>,
   private void execute(Stmt stmt)
   {
     stmt.accept(this);
+  }
+
+  void executeBlock(List<Stmt> statements,
+                    Environment environment)
+  {
+    Environment previous = this.environment;
+    try
+    {
+      this.environment = environment;
+      
+      for (Stmt statement : statements)
+        execute(statement);
+    }
+    finally
+    {
+      this.environment = previous;
+    }
+  }
+
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt)
+  {
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt)
+  {
+    Object value = null;
+    if (stmt.initializer != null)
+      value = evalute(stmt.initializer);
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr)
+  {
+    Object value = evalute(expr.value);
+    environment.assign(expr.name, value);
+    return value;
   }
 
   private boolean isTruthy(Object object)
